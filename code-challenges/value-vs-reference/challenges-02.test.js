46ee730cf6f2633f47e4168f124a51a75bd551ee
Sean Murray <sean.j.murray1@gmail.com>
2020-06-02 20:16:02 -0700

    Finish stretch 2

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -93,8 +93,7 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  
-
+  arr2.forEach(x => arr1.push(x))
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -153,7 +152,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
